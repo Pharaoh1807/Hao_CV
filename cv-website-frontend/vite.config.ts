df62c88ab76847bdeea0fdd362179ceb68a6835b
Nguyen Thanh Hao <nguyenthanhhao@PharaohMac.lan>
2026-05-12 21:11:51 +0700

    Fix base path in vite.config.ts

--- cv-website-frontend/vite.config.ts
+++ cv-website-frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Sử dụng PostCSS để xử lý Tailwind v4 (theo thông báo lỗi)
 export default defineConfig({
   plugins: [react()],
-  base: '/mycv/',
+  base: '/Hao_CV/',
 })
